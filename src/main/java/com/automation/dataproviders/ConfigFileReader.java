25407dc5012401ebf39e81b7fec9182ea552d2d0
anastasiiakhartynova <anastasia.khartynova@gmail.com>
2020-12-28 15:12:03 +0200

    Created class to read properties file

---
+++ src/main/java/com/automation/dataproviders/ConfigFileReader.java
@@ -0,0 +1,58 @@
+package com.automation.dataproviders;
+
+import java.io.*;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConfigFileReader {
+
+    private Properties properties;
+    private final static String fileName = "config.properties";
+    private final static Logger LOGGER = Logger.getLogger(ConfigFileReader.class.getName());
+
+    public ConfigFileReader() {
+        ClassLoader classLoader = ConfigFileReader.class.getClassLoader();
+        try {
+            File file = new File(classLoader.getResource(fileName).getFile());
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                properties = new Properties();
+                properties.load(reader);
+            }
+        } catch (NullPointerException | IOException ex) {
+            LOGGER.log(Level.SEVERE, "Could not read configuration file " + fileName, ex);
+        }
+    }
+
+    public String getUsername() {
+        String username = properties.getProperty("username");
+        if (username != null)
+            return username;
+        else
+            throw new RuntimeException("Username not specified in the config.properties file");
+    }
+
+    public String getPassword() {
+        String password = properties.getProperty("password");
+        if (password != null)
+            return password;
+        else
+            throw new RuntimeException("Password not specified in the config.properties file");
+    }
+
+    public String getGmailUrl() {
+        String gmailUrl = properties.getProperty("gmailUrl");
+        if (gmailUrl != null)
+            return gmailUrl;
+        else
+            throw new RuntimeException("Gmail url time not specified in the config.properties file");
+    }
+
+    public int getWait() {
+        String wait = properties.getProperty("wait");
+        if (wait != null)
+            return Integer.parseInt(wait);
+        else
+            throw new RuntimeException("Wait time not specified in the config.properties file");
+    }
+}
