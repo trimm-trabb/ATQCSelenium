af1d780c1ca9745b52c26c5dad597fb53be734b5
anastasiiakhartynova <anastasia.khartynova@gmail.com>
2021-01-21 21:52:43 +0200

    Added testng listener

---
+++ src/main/java/com/automation/driver/WebDriverListener.java
@@ -0,0 +1,28 @@
+package com.automation.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
+import org.testng.ITestResult;
+
+public class WebDriverListener implements IInvokedMethodListener {
+
+    @Override
+    public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
+        if (method.isTestMethod()) {
+            String browser = method.getTestMethod().getXmlTest().getLocalParameters().get("browser");
+            WebDriver driver = DriverFactory.getDriverInstance(browser);
+            WebDriverManager.setWebDriver(driver);
+        }
+    }
+
+    @Override
+    public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+        if (method.isTestMethod()) {
+            WebDriver driver = WebDriverManager.getDriver();
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+    }
+}
